c900014e6f1bc620dd4842b7bfc0a294659be631
DmitryIgnatev <dmitryignatevdev@gmail.com>
2023-10-11 13:15:36 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:twilio_video_calls/room/join_room_page.dart';
+
+void main() {
+  runApp(const TwilioProgrammableVideoExample());
+}
+
+class TwilioProgrammableVideoExample extends StatelessWidget {
+  const TwilioProgrammableVideoExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: JoinRoomPage(),
+    );
+  }
+}
